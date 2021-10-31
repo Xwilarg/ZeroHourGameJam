800b3533fc1881869f042e2203f33c88ce4931ec
Xwilarg <xwilarg@yahoo.fr>
2021-10-31 02:07:06 +0200

    Allow to change gravity with WASD

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,25 +10,25 @@ public class GameManager : MonoBehaviour
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
             if (_currDir != Direction.Up) Physics.gravity = Vector3.up;
             else Physics.gravity += Vector3.up;
             _currDir = Direction.Up;
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
         {
             if (_currDir != Direction.Down) Physics.gravity = Vector3.down;
             else Physics.gravity += Vector3.down;
             _currDir = Direction.Down;
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.A))
         {
             if (_currDir != Direction.Right) Physics.gravity = Vector3.right;
             else Physics.gravity += Vector3.right;
             _currDir = Direction.Right;
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.D))
         {
             if (_currDir != Direction.Left) Physics.gravity = Vector3.left;
             else Physics.gravity += Vector3.left;
