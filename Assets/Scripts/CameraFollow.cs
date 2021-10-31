b4214e85c46b7ee435f455dcf5746abface13381
Xwilarg <xwilarg@yahoo.fr>
2021-10-31 02:37:24 +0200

    Add camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _follow;
+
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        _offset = transform.position - _follow.transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = _offset + _follow.transform.position;
+    }
+}
