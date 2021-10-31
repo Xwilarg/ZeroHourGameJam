2c77538107f5d7d723735af64fd315fb68ca8a4b
Xwilarg <xwilarg@yahoo.fr>
2021-10-31 02:49:42 +0200

    Add sasests

--- Assets/Scripts/ObjectiveReceiver.cs
+++ Assets/Scripts/ObjectiveReceiver.cs
@@ -2,5 +2,23 @@
 
 public class ObjectiveReceiver : MonoBehaviour
 {
-    public int Order;
+    public int Id;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Receiver"))
+        {
+            var obj = collision.collider.GetComponent<ObjectiveReceiver>();
+            if (obj.Id == Id)
+            {
+                GameManager.S.Left--;
+                if (GameManager.S.Left == 0)
+                {
+                    GameManager.S.Win();
+                }
+                Destroy(collision.collider.gameObject);
+                Destroy(gameObject);
+            }
+        }
+    }
 }
