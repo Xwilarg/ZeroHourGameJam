b35cdc79daf22631f42aab85bbb9fa82e669623b
Xwilarg <xwilarg@yahoo.fr>
2021-10-31 02:33:04 +0200

    Add thumbs

---
+++ Assets/Scripts/ThumbCrate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ThumbCrate : MonoBehaviour
+{
+    private AudioSource _audio;
+
+    private void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _audio?.Play();
+    }
+}
